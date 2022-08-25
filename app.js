93aa644aea6544f161859cc898c7bcc5fb019543
Jitendra Patel <90861924+comrade1120@users.noreply.github.com>
2022-08-25 08:16:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose.connect(
+    "mongodb+srv://admin:Jitu1120@cluster0.8xv8c7d.mongodb.net/booksStore?retryWrites=true&w=majority"
+  ).then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  }).catch((err) => console.log(err));
